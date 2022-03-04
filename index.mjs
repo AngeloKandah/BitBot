20506545adf903fbeb49221fa2cbc8ddb8e1f724
AngeloKandah <angelokandah@gmail.com>
2022-03-03 17:40:35 -0800

    Added Postgres Query and Nodemon

--- index.mjs
+++ index.mjs
@@ -9,7 +9,9 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.join(__dirname, '/.env') });
 
 // Discord Client Login
-const discordClient = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
+const discordClient = new Discord.Client({
+  intents: ['GUILDS', 'GUILD_MESSAGES'],
+});
 await discordClient.login(process.env.DISCORD_TOKEN);
 console.log('Successfully Connected to Bot');
 
@@ -20,18 +22,34 @@ console.log('Successfully Connected to Postgres');
 
 const prefix = '!';
 
-discordClient.on('messageCreate', (message) => {
+discordClient.on('messageCreate', async (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) {
     return;
   }
-  const commandBody = message.content.slice(prefix.length);
-  const args = commandBody.split(' ');
-  const command = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).trim().split(/\s+/);
+  const [command, ...params] = args;
   if (command === 'test') {
     message.channel.send('test success');
   }
 
   if (command === 'givebits') {
-    message.channel.send('bits given to:');
+    const [_, bits, ...rest] = params;
+    if (!message.mentions.users.size) {
+      return message.reply('Tag someone next time or no bits.');
+    }
+    if (!bits) {
+      return message.reply('Missing Certain Params');
+    }
+    const dbQuery = `
+      INSERT INTO bitbot(user_id, bits) 
+      VALUES($1, $2)
+      ON CONFLICT (user_id) 
+      DO 
+      UPDATE SET bits = EXCLUDED.bits + bitbot.bits`;
+    const { id } = message.mentions.users.first();
+    console.log(id);
+    const valuesForQuery = [id, bits];
+    const res = await database.query(dbQuery, valuesForQuery);
+    console.log(res.rows[0]);
   }
 });
