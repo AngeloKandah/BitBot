3e6a2858f8b66854e91305a0c656c804e489d2d5
AngeloKandah <angelokandah@gmail.com>
2022-03-05 14:20:58 -0800

    Minor update to path.join and destructuring

--- index.mjs
+++ index.mjs
@@ -6,7 +6,7 @@ import postgres from 'pg';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.join(__dirname, '/.env') });
+dotenv.config({ path: path.join(__dirname, './.env') });
 
 // Discord Client Login
 const discordClient = new Discord.Client({
@@ -22,34 +22,45 @@ console.log('Successfully Connected to Postgres');
 
 const prefix = '!';
 
+// Change user id to not be baseline make it so there is auto increment
+
 discordClient.on('messageCreate', async (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) {
     return;
   }
   const args = message.content.slice(prefix.length).trim().split(/\s+/);
+  const {
+    author: { id: idOfAuthor },
+  } = message;
   const [command, ...params] = args;
   if (command === 'test') {
     message.channel.send('test success');
   }
+  /* if (command === 'addAdmin' && (idOfAuthor === process.env.ADMIN || true)) {
+    const dbQuery = `
+      SELECT * FROM bitbot 
+      WHERE user_id = ANY(VALUES($1));`;
+    const valuesForQuery = [idOfAuthor];
+    console.log(await database.query(dbQuery, valuesForQuery));
+
+  } */
 
   if (command === 'givebits') {
-    const [_, bits, ...rest] = params;
     if (!message.mentions.users.size) {
       return message.reply('Tag someone next time or no bits.');
     }
+    const [raw_id, bits, ...rest] = params;
+    const id = raw_id.replace(/[^0-9]*/g, '');
     if (!bits) {
-      return message.reply('Missing Certain Params');
+      return message.reply('Missing Bit Amount.');
     }
     const dbQuery = `
-      INSERT INTO bitbot(user_id, bits) 
-      VALUES($1, $2)
+      INSERT INTO bitbot(id, user_id, bits) 
+      VALUES(DEFAULT, $1, $2)
       ON CONFLICT (user_id) 
       DO 
-      UPDATE SET bits = EXCLUDED.bits + bitbot.bits`;
-    const { id } = message.mentions.users.first();
-    console.log(id);
+        UPDATE SET bits = EXCLUDED.bits + bitbot.bits`;
     const valuesForQuery = [id, bits];
-    const res = await database.query(dbQuery, valuesForQuery);
-    console.log(res.rows[0]);
+    await database.query(dbQuery, valuesForQuery);
   }
 });
