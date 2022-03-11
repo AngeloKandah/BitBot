cb59dc74b30b8ca64d4b02d785d1b31d325bbfbb
AngeloKandah <angelokandah@gmail.com>
2022-03-10 16:26:23 -0800

    Added create tables upon connection

--- index.mjs
+++ index.mjs
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import Discord from 'discord.js';
 import postgres from 'pg';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,13 +16,25 @@ await discordClient.login(process.env.DISCORD_TOKEN);
 console.log('Successfully Connected to Bot');
 
 // Postgres Connection
+// Change database.connect() object, i think just change .env, colby sent website
 const database = new postgres.Client();
 await database.connect();
 console.log('Successfully Connected to Postgres');
+await database.query(
+  `CREATE TABLE IF NOT EXISTS admin(
+    id SERIAL,
+    user_id TEXT UNIQUE
+  );`
+);
+await database.query(
+  `CREATE TABLE IF NOT EXISTS bits(
+    id SERIAL,
+    user_id TEXT UNIQUE,
+    bits INTEGER
+  );`
+);
 
 const prefix = '!';
-
-// Change user id to not be baseline make it so there is auto increment
 
 discordClient.on('messageCreate', async (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) {
@@ -39,11 +51,13 @@ discordClient.on('messageCreate', async (message) => {
       return message.reply('Missing new admin to add.');
     }
     const dbQueryForUsers = `
-      SELECT user_id FROM bitbotadmin;
+      SELECT user_id FROM admin;
     `;
     const { rows } = await database.query(dbQuery);
-    const commandRunByAdmin = rows.some(({ user_id }) => user_id === idOfAuthor);
-    if (!commandRunByAdmin) {
+    const isCommandRanByAdmin = rows.some(
+      ({ user_id }) => user_id === idOfAuthor
+    );
+    if (!isCommandRanByAdmin) {
       return;
     }
   }
@@ -59,14 +73,22 @@ discordClient.on('messageCreate', async (message) => {
     }
     const dbQuery = `
       INSERT INTO 
-        bitbot(id, user_id, bits) 
+        bits(id, user_id, bits) 
       VALUES 
         (DEFAULT, $1, $2)
       ON CONFLICT (user_id) DO 
         UPDATE 
-          SET bits = EXCLUDED.bits + bitbot.bits;
+          SET bits = EXCLUDED.bits + bits.bits;
     `;
     const valuesForQuery = [id, bits];
     await database.query(dbQuery, valuesForQuery);
   }
+});
+
+//This will be removed upon finalizing, purely for testing and resetting.
+process.once('SIGHUP', async () => {
+  await database.query(`DROP TABLE IF EXISTS admin, bits`);
+  console.log('Successfully dropped tables');
+  await database.end();
+  return;
 });
